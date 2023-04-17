3f16a217c67e142f3df21e187a4fb3a49ac8d412
hoang thanh nghia <thanhnghia1712@gmail.com>
2023-04-17 16:43:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  unstable_HistoryRouter as HistoryRouter,
+  Router,
+} from "react-router-dom";
+
+import HomeBooking from "./features/Booking/HomeBooking";
+import RouteComponent from "./HOCs/RouteComponent";
+import Signin from "./features/Auth/Signin";
+import Category from "./features/Booking/components/Category";
+import Signup from "./features/Auth/Signup";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchProfile } from "./features/Auth/thunk";
+import CourseListMySefl from "./features/Booking/CourseListMySefl";
+import User from "./features/Admin/User";
+
+import Course from "./features/Admin/Course";
+
+function App() {
+  const dispatch = useDispatch();
+  const {user} = useSelector(state=>state.auth);
+    
+  useEffect(() => {
+    dispatch(fetchProfile);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<RouteComponent Component={HomeBooking} />}
+        ></Route>
+        <Route
+          path="/signin"
+          element={
+            <RouteComponent Component={Signin} isAuth={true} redirectPath="/" />
+          }
+        ></Route>
+        <Route
+          path="/signup"
+          element={<RouteComponent Component={Signup} />}
+        ></Route>
+        <Route
+          path="/course-list"
+          element={<RouteComponent Component={CourseListMySefl} />}
+        ></Route>
+        <Route
+          path="/danhmuckhoahoc/:id"
+          element={<RouteComponent Component={Category} />}
+        ></Route>
+        
+        <Route
+          path="/admin"
+          element={
+            <RouteComponent
+              Component={User}
+              isAdmin={true}
+              redirectPath="/signin"
+            />
+          }
+        ></Route>
+
+        <Route
+          path="/admin/user"
+          element={
+            <RouteComponent
+              Component={User}
+              isAdmin={true}
+              redirectPath="/signin"
+            />
+          }
+        ></Route>
+        <Route
+          path="/admin/user:page"
+          element={
+            <RouteComponent
+              Component={User}
+              isAdmin={true}
+              redirectPath="/signin"
+            />
+          }
+        ></Route>
+        <Route
+          path="/admin/course"
+          element={
+            <RouteComponent
+              Component={Course}
+              isAdmin={true}
+              redirectPath="/signin"
+            />
+          }
+        ></Route>
+        <Route
+          path="/admin/course:page"
+          element={
+            <RouteComponent
+              Component={Course}
+              isAdmin={true}
+              redirectPath="/signin"
+            />
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/features/Booking/HomeBooking.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import Header from '../../components/Header'
+import CourseList from './components/CourseList'
+import { fetchCourseList } from './thunk';
+import Footer from '../../components/Footer';
+
+const HomeBooking = () => {
+  const dispatch = useDispatch();
+  const [searchParam, setSearchParam] = useSearchParams();
+  useEffect(() => {    
+    dispatch(fetchCourseList(searchParam.get("page")));
+  }, [dispatch, searchParam.get("page")]);
+  return (
+    <div>
+      <Header></Header>
+      <CourseList></CourseList>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default HomeBooking
